b1b295e46286af7b0358c0bcad7d44f47b2ccb10
str4tox <ihuerta@udec.cl>
2018-05-13 02:44:45 -0400

    callendar functions

--- src/calendar.js
+++ src/calendar.js
@@ -3,34 +3,37 @@ import events from './events'
 import 'react-big-calendar/lib/css/react-big-calendar.css'
 import BigCalendar from 'react-big-calendar';
 import moment from 'moment';
+require('globalize/lib/cultures/globalize.culture.es')
 
 BigCalendar.setLocalizer(BigCalendar.momentLocalizer(moment))
 
-let Selectable = () => (
-  <React.Fragment>
-    <h3 className="callout">
-      Click an event to see more info, or drag the mouse over the calendar to
-      select a date/time range.
-    </h3>
-    <BigCalendar
-      selectable
-      events={events}
-      toolbar={true} //muestra la barrita arriba del calendario
-      step={15} //tamano de la seleccion
-      timeslots={4} //tamano de los slots
-      defaultView="week"
-      scrollToTime={new Date(1970, 1, 1, 6)}
-      defaultDate={new Date}
-      onSelectEvent={event => alert(event.title)}
-      onSelectSlot={slotInfo =>
-        alert(
-          `selected slot: \n\nstart ${slotInfo.start.toLocaleString()} ` +
-            `\nend: ${slotInfo.end.toLocaleString()}` +
-            `\naction: ${slotInfo.action}`
+class Calendar extends Component {
+    render(){
+        return(
+          <React.Fragment>
+            <BigCalendar
+              startAccessor='startDate'
+              endAccessor='endDate'
+              selectable
+              events={events}
+              weekends={false}
+              toolbar={true} //muestra la barrita arriba del calendario
+              step={15} //tamano de la seleccion
+              timeslots={4} //tamano de los slots
+              defaultView="week"
+              scrollToTime={new Date(1970, 1, 1, 6)}
+              defaultDate={new Date}
+              onSelectEvent={event => alert(event.title)}
+              onSelectSlot={slotInfo =>
+                alert(
+                  `selected slot: \n\nstart ${slotInfo.start.toLocaleString()} ` +
+                    `\nend: ${slotInfo.end.toLocaleString()}` +
+                    `\naction: ${slotInfo.action}`
+                )
+              }
+            />
+          </React.Fragment>
         )
-      }
-    />
-  </React.Fragment>
-)
-
-export default Selectable
+    }
+}
+export default Calendar
