c9783b68af98692e3e65ccb2ed45ed41f49bcd29
Joaquin Cardenas <jcardenas.lie@gmail.com>
2018-05-13 00:11:09 -0400

    commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,32 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
+import CelebrityJokes from './components/CelebrityJokes';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+
+/*
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CelebrityJokes from './components/CelebrityJokes';
+import FoodJokes from './components/FoodJokes';
+import { Router, Route, browserHistory } from 'react-router';
+
+const Root = () => {
+  return (
+    <div className="container">
+      <Router history={browserHistory}>
+        <Route path="/" component={FoodJokes}/>
+        <Route path="/special" component={CelebrityJokes}/>
+      </Router>
+    </div>
+  )
+}
+
+ReactDOM.render(<Root />, document.getElementById('root'));
+*/
